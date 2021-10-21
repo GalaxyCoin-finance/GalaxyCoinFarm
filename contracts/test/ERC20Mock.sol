a0f5520361bbe4252de9f54ac2905e62a2b53ab3
humanshield85 <rachidboudjelida@gmail.com>
2021-10-21 22:49:20 +0100

    Setup the deployment scripts for all major networks and local tests

---
+++ contracts/test/ERC20Mock.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract ERC20Mock is ERC20 {
+    constructor (
+        string memory name,
+        string memory symbol,
+        uint256 initialBalance
+    ) payable ERC20(name, symbol) {
+        _mint(msg.sender, initialBalance);
+    }
+
+    function transferInternal(address from, address to, uint256 value) public {
+        _transfer(from, to, value);
+    }
+
+    function approveInternal(address owner, address spender, uint256 value) public {
+        _approve(owner, spender, value);
+    }
+}
--- truffle-config.js
+++ truffle-config.js
@@ -1,71 +1,95 @@
-require('dotenv').config();
-
 const HDWalletProvider = require('@truffle/hdwallet-provider');
-mnemonic = process.env.MNEMONIC;
+const NonceTrackerSubprovider = require("web3-provider-engine/subproviders/nonce-tracker")
+
+require('dotenv').config();
+const secrets = require('./secrets.json');
+
 module.exports = {
-  networks: {
-    development: {
-      network_id: '*',
-      host: 'localhost',
-      port: process.env.PORT
+    networks: {
+        development: {
+            network_id: '*',
+            host: 'localhost',
+            port: process.env.PORT
+        },
+        polygon: {
+            provider: function () {
+                const wallet = new HDWalletProvider(secrets.polygon.mnemonic, secrets.polygon.rpcURL)
+                const nonceTracker = new NonceTrackerSubprovider()
+                wallet.engine._providers.unshift(nonceTracker)
+                nonceTracker.setEngine(wallet.engine)
+                return wallet
+            },
+            network_id: 137,
+            confirmations: 2,
+            //websockets: true
+        },
+        mumbai: {
+            provider: function () {
+                const wallet = new HDWalletProvider(secrets.mumbai.mnemonic, secrets.mumbai.rpcURL)
+                const nonceTracker = new NonceTrackerSubprovider()
+                wallet.engine._providers.unshift(nonceTracker)
+                nonceTracker.setEngine(wallet.engine)
+                return wallet
+            },
+            network_id: 80001,
+            confirmations: 2,
+            //websockets: true
+        },
+        kovan: {
+            provider: function () {
+                const wallet = new HDWalletProvider(secrets.kovan.mnemonic, secrets.kovan.rpcURL)
+                const nonceTracker = new NonceTrackerSubprovider()
+                wallet.engine._providers.unshift(nonceTracker)
+                nonceTracker.setEngine(wallet.engine)
+                return wallet
+            },
+            network_id: 42,
+            gas: 12450000,
+            gasPrice: 20000000000,
+            confirmations: 2,
+            skipDryRun: true,
+            //websockets: true
+        },
+        bsctestnet: {
+            provider: function () {
+                const wallet = new HDWalletProvider(secrets.bsctestnet.mnemonic, secrets.bsctestnet.rpcURL)
+                const nonceTracker = new NonceTrackerSubprovider()
+                wallet.engine._providers.unshift(nonceTracker)
+                nonceTracker.setEngine(wallet.engine)
+                return wallet
+            },
+            network_id: 97,
+            confirmations: 2,
+            timeoutBlocks: 2000,
+            networkCheckTimeout: 1000000000,
+            skipDryRun: true
+        },
+        bsc: {
+            provider: function () {
+                const wallet = new HDWalletProvider(secrets.bsc.mnemonic, secrets.bsc.rpcURL)
+                const nonceTracker = new NonceTrackerSubprovider()
+                wallet.engine._providers.unshift(nonceTracker)
+                nonceTracker.setEngine(wallet.engine)
+                return wallet
+            },
+            network_id: 56,
+            confirmations: 20,
+            timeoutBlocks: 2000,
+            networkCheckTimeout: 1000000000,
+            skipDryRun: true
+        }
     },
-    main: {
-      provider: function() {
-        return new HDWalletProvider(
-          //private keys array
-          process.env.MNEMONIC,
-          //url to ethereum node
-          process.env.WEB3_PROVIDER_ADDRESS
-        )
-      },
-      network_id: 1,
-      gas: 4000000,
-      gasPrice: 200000000000,
-      confirmations: 2,
-      websockets: true
+    compilers: {
+        solc: {
+            version: "0.8.4",
+        }
     },
-    kovan: {
-      provider: function() {
-        return new HDWalletProvider(
-          //private keys array
-          process.env.MNEMONIC,
-          //url to ethereum node
-          process.env.WEB3_PROVIDER_ADDRESS
-        )
-      },
-      network_id: 42,
-      gas: 12450000,
-      gasPrice: 20000000000,
-      confirmations: 2,
-      websockets: true
-    },
-    bsctestnet: {
-      provider: () => new HDWalletProvider(mnemonic, `https://data-seed-prebsc-1-s1.binance.org:8545`),
-      network_id: 97,
-      confirmations: 10,
-      timeoutBlocks: 2000,
-      networkCheckTimeout: 1000000000 ,
-      skipDryRun: true
-    },
-    bsc: {
-      provider: () => new HDWalletProvider(mnemonic, `https://bsc-dataseed1.binance.org`),
-      network_id: 56,
-      confirmations: 20,
-      timeoutBlocks: 2000,
-      networkCheckTimeout: 1000000000 ,
-      skipDryRun: true
+    plugins: [
+        'truffle-plugin-verify'
+    ],
+    api_keys: {
+        bscscan: secrets.bsc.explorerApiKEy,
+        etherscan: secrets.kovan.explorerApiKEy,
+        polygonscan: secrets.polygon.explorerApiKEy
     }
-  },
-  compilers: {
-    solc: {
-      version: "0.8.4",
-    }
-  },
-  plugins: [
-    'truffle-plugin-verify'
-  ],
-  api_keys: {
-    bscscan: process.env.BSCSCAN_API
-    
-  }
 };
