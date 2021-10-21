3b2794e8680763beb657ee48c955731cad07c735
humanshield85 <rachidboudjelida@gmail.com>
2021-10-21 21:46:08 +0100

    - Fix Issues raise by Sirluke. - Added option to initialize Farm after deployment for more flexibility and control. - Make AdminWallet changeable by owner. - Add claim fee to the contract.

--- contracts/Farm.sol
+++ contracts/Farm.sol
@@ -71,6 +71,8 @@ contract Farm is Ownable {
     //admin wallet's address
     address private adminWallet;
 
+    // used to track if the farm is initialized 
+    bool private init;
     event Deposit(address indexed user, uint256 indexed pid, uint256 amount);
     event Withdraw(address indexed user, uint256 indexed pid, uint256 amount);
     event EmergencyWithdraw(address indexed user, uint256 indexed pid, uint256 amount);
@@ -81,16 +83,47 @@ contract Farm is Ownable {
         startBlock = _startBlock;
         endBlock = _startBlock;
         adminWallet = _adminWalletAddr;
+
+        init = _erc20 != IERC20(address(0)) &&
+            endBlock != 0 && _startBlock != 0;
     }
+
+
 
     // Number of LP pools
     function poolLength() external view returns (uint256) {
         return poolInfo.length;
     }
 
+    function initializeFarm(
+        IERC20 _erc20,
+         uint256 _rewardPerBlock,
+          uint256 _startBlock,
+           address _adminWalletAddr
+    ) external onlyOwner {
+        require(!init ,"initializeFarm: Already initialized");
+
+        erc20 = _erc20;
+        rewardPerBlock = _rewardPerBlock;
+        startBlock = _startBlock;
+        endBlock = _startBlock;
+        adminWallet = _adminWalletAddr;
+
+        init = _erc20 != IERC20(address(0)) &&
+            endBlock != 0 && _startBlock != 0;
+    }
+
+
+    // change adminWallet (the one that receives the fees)
+    function changeAdminWallet(address _newAdminWallet) external onlyOwner {
+        require(_newAdminWallet != address(0), "changeAdminWallet: can't be zero address");
+        adminWallet = _newAdminWallet;
+    }
+
     // Fund the farm, increase the end block
     function fund(uint256 _amount) external onlyOwner {
         require(block.number < endBlock, "fund: too late, the farm is closed");
+        require(_amount.mod(rewardPerBlock) == 0, "fund: _amount not dividable by rewardPerBlock");         // avoid precision loss
 
         erc20.transferFrom(address(msg.sender), address(this), _amount);
         endBlock += _amount.div(rewardPerBlock);
@@ -108,7 +141,7 @@ contract Farm is Ownable {
             allocPoint: _allocPoint,
             lastRewardBlock: lastRewardBlock,
             accERC20PerShare: 0,
-            withdrawFee: _withdrawFee, 
+            withdrawFee: _withdrawFee,
             claimFee: _claimFee,
             stakedAmount: 0
         }));
@@ -210,10 +243,17 @@ contract Farm is Ownable {
     function withdraw(uint256 _pid, uint256 _amount) external {
         PoolInfo storage pool = poolInfo[_pid];
         UserInfo storage user = userInfo[_pid][msg.sender];
+
         require(user.amount >= _amount, "withdraw: can't withdraw more than deposit");
+
         updatePool(_pid);
+
         uint256 pendingAmount = user.amount.mul(pool.accERC20PerShare).div(1e36).sub(user.rewardDebt);
-        erc20Transfer(msg.sender, pendingAmount);
+        uint256 adminRewardAmount = pendingAmount.mul(pool.claimFee).div(1000);
+        if(adminRewardAmount > 0)
+            erc20Transfer(adminWallet, adminRewardAmount);
+        erc20Transfer(msg.sender, pendingAmount - adminRewardAmount);
+
         user.amount = user.amount.sub(_amount);
         user.rewardDebt = user.amount.mul(pool.accERC20PerShare).div(1e36);
         uint256 adminWalletAmount = _amount * pool.withdrawFee / 1000;
@@ -277,17 +317,21 @@ contract Farm is Ownable {
         require(block.number < endBlock, "changeRewardPerBlock: Too late farming ended");
         uint256 leftRewards = rewardPerBlock.mul(endBlock - startBlock) - (block.number > startBlock ? rewardPerBlock.mul( block.number-startBlock) : 0);
         uint256 newLeftBlocks = leftRewards.div(_rewardPerBlock);
-        uint256 newEndBlock = block.number > startBlock ? block.number : startBlock + newLeftBlocks;
+        uint256 leftoverRewards = leftRewards.mod(_rewardPerBlock);
+        uint256 newEndBlock = block.number > startBlock ? block.number + newLeftBlocks : startBlock + newLeftBlocks;
 
         if(_rewardPerBlock > rewardPerBlock)
-            // 21600 blocks should be roughly 24 hours on matic
+            // 21600 blocks should be roughly 24 hours
             require(newEndBlock > block.number + 21600,"Please fund the contract before increasing the rewards per block" );
-        
+
         if (_withUpdate)
             massUpdatePools();
 
         endBlock = newEndBlock;
         rewardPerBlock = _rewardPerBlock;
+        // send any excess rewards to admin (caused by rewards % rewardperblock != 0)
+        if(leftoverRewards > 0)
+            erc20Transfer(adminWallet, leftoverRewards);
     }
 
 
